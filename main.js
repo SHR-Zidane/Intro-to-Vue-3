b656074c8b5696f77c41fc6a0db101e4656cd337
SHR-Zidane <pd01wvj@eduvaud.ch>
2026-01-14 10:37:44 +0100

    ex

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return{
+            product: 'Socks',
+
+            description: 'chaussettes en coton',
+
+            image: './assets/images/socks_green.jpg',
+
+            inventory: 8,
+
+            details: ['50% cotton', '30% wool', '20% polyester'],
+
+            onSale: true,
+
+            variants: [
+                { id:2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            ],
+
+            size: [
+                {id:1, size: 'S'},
+                {id:2, size: 'M'},
+                {id:3, size: 'L'},
+                {id:4, size: 'XL'}
+            ],
+
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
+});
